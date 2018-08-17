03ec511566f8ef3646c6b16927050b2b429539b1
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-17 21:49:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ client.on("ready", () => {
   client.user.setActivity(`+snap for ${client.users.size} members in ${client.guilds.size} servers.`);
 });
 
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`+snap for ${client.users.size} members in ${client.guilds.size} servers.`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`+snap for ${client.users.size} members in ${client.guilds.size} servers.`);
+});
+
 client.on('message', (message) => {
     if(message.content == '+snap'){
         var meme = Math.floor((Math.random() * 24) + 1);
