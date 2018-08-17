2089527e6e106de3608b8e20e4959ecb034f31a8
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-17 20:50:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const client = new Discord.Client({ fetchAllMembers: true, sync: true });
 const config = require('./config.json');
 client.config = config;
 
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setActivity(`Snapchat`);
+});
