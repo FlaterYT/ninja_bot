6884385458552581b926c258aeb15982024b1946
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-17 21:43:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,171 @@ client.on("ready", () => {
 });
 
 client.on('message', (message) => {
-    if(message.content === "+snap") {
-       number = 24;
-       imageNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
-       message.channel.send(new Discord.Attachment('./images/', '.png') )
+    if(message.content == '+snap'){
+        var meme = Math.floor((Math.random() * 24) + 1);
+       
+      
+      
+      if (meme == 1) {
+   message.channel.send({
+            file: "https://image.ibb.co/cJ743K/1.jpg" 
+             });
+}
+       if (meme == 2) {
+   message.channel.send({
+            file: "https://image.ibb.co/dDu6He/2.png" 
+             });
+}
+      
+        if (meme == 3) {
+   message.channel.send({
+            file: "https://image.ibb.co/jH6xOK/3.jpg" 
+             });
+}
+      
+       if (meme == 4) {
+   message.channel.send({
+            file: "https://image.ibb.co/cauTVz/4.jpg" 
+             });
+}
+      
+      if (meme == 5) {
+   message.channel.send({
+            file: "https://image.ibb.co/mEO0ce/5.jpg" 
+             });
+}
+      
+      if (meme == 6) {
+   message.channel.send({
+            file: "https://image.ibb.co/c7KvAz/6.jpg" 
+             });
+}
+      if (meme == 7) {
+   message.channel.send({
+            file: "https://image.ibb.co/mtnhqz/7.jpg" 
+             });
+}
+      
+      
+            if (meme == 8) {
+   message.channel.send({
+            file: "https://image.ibb.co/fiGsqz/8.jpg" 
+             });
+}			 
+      
+      
+            if (meme == 9) {
+   message.channel.send({
+            file: "https://image.ibb.co/ktSyVz/9.jpg" 
+             });
+}  
+      
+      
+            if (meme == 10) {
+   message.channel.send({
+            file: "https://image.ibb.co/fGSbHe/10.jpg" 
+             });
+} 
+      
+      
+            if (meme == 11) {
+   message.channel.send({
+            file: "https://image.ibb.co/mgHMiK/11.jpg" 
+             });
+}      
+      
+      
+            if (meme == 12) {
+   message.channel.send({
+            file: "https://image.ibb.co/jebE3K/12.jpg" 
+             });
+} 
+      
+      
+            if (meme == 13) {
+   message.channel.send({
+            file: "https://image.ibb.co/nsHbHe/13.jpg" 
+             });
+} 
+      
+      
+            if (meme == 14) {
+   message.channel.send({
+            file: "https://image.ibb.co/mNB3xe/14.jpg" 
+             });
+} 
+      
+      
+            if (meme == 15) {
+   message.channel.send({
+            file: "https://image.ibb.co/e38SOK/15.jpg" 
+             });
+} 
+      
+      
+            if (meme == 16) {
+   message.channel.send({
+            file: "https://image.ibb.co/c1JGHe/16.jpg" 
+             });
+} 
+      
+      
+            if (meme == 17) {
+   message.channel.send({
+            file: "https://image.ibb.co/mG1giK/17.jpg" 
+             });
+} 
+      
+      
+            if (meme == 18) {
+   message.channel.send({
+            file: "https://image.ibb.co/cHVVce/18.jpg" 
+             });
+} 
+      
+      
+            if (meme == 19) {
+   message.channel.send({
+            file: "https://image.ibb.co/n5hyVz/19.jpg" 
+             });
+} 
+      
+      
+            if (meme == 20) {
+   message.channel.send({
+            file: "https://image.ibb.co/iyYSOK/20.jpg" 
+             });
+} 
+      
+      
+            if (meme == 21) {
+   message.channel.send({
+            file: "https://image.ibb.co/gwsMiK/21.jpg" 
+             });
+} 
+      
+      
+            if (meme == 22) {
+   message.channel.send({
+            file: "https://image.ibb.co/bwi5Az/22.jpg" 
+             });
+} 
+      
+      
+            if (meme == 23) {
+   message.channel.send({
+            file: "https://image.ibb.co/nkdGHe/23.jpg" 
+             });
+} 
+      
+      
+            if (meme == 24) {
+   message.channel.send({
+            file: "https://image.ibb.co/h0vwHe/24.jpg" 
+             });
+} 
+      
     }
-
-
 });
 
 client.login(process.env.B0T_T0KEN);
