4910936eef4d074a98a666c061f603c327b43183
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-19 18:48:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const config = require('./config.json');
 client.config = config;
 
 client.on("ready", () => {
+	  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  client.user.setActivity(`+help for ${client.users.size} members in ${client.guilds.size} servers.`);
   // Check if the table "points" exists.
   const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
   if (!table['count(*)']) {
@@ -119,25 +121,6 @@ client.on("message", message => {
     return message.channel.send({embed});
   }
   
-});
-
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
-  client.user.setActivity(`+help for ${client.users.size} members in ${client.guilds.size} servers.`);
-});
-
-client.on("guildCreate", guild => {
-  // This event triggers when the bot joins a guild.
-  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`+help for ${client.users.size} members in ${client.guilds.size} servers.`);
-});
-
-client.on("guildDelete", guild => {
-  // this event triggers when the bot is removed from a guild.
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`+help for ${client.users.size} members in ${client.guilds.size} servers.`);
 });
 
 
