847b132cd076a2c205138f68929e1ba52f8cfb47
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-19 10:54:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -426,32 +426,4 @@ client.on('message', (message) => {
     }
 });
 
-
-
-client.on("message", message => {
-  var args = message.content.split(" ").slice(1);
-
-  if (message.content.startsWith(config.prefix + "eval")) {
-    if(message.author.id !== config.ownerID) return;
-    try {
-      var code = args.join(" ");
-      var evaled = eval(code);
-
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
-
-      message.channel.sendCode(clean("x1", clean(evaled));
-    } catch (err) {
-      message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    }
-  }
-});
-	
-function clean(text) {
-  if (typeof(text) === "string")
-    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-  else
-      return text;
-}
-
 client.login(process.env.B0T_T0KEN);
