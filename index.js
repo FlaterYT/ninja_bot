dd17b62ff0ae802e177b4d88d20a795e455168e9
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-19 17:58:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -62,8 +62,7 @@ client.on("message", message => {
     sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
       if (!row) return message.reply("sadly you do not have any points yet!");
       message.reply(`you currently have ${row.points} points, good going!`);
-    });
-  }
+    }
 });
 
 client.on('message', (message) => {
