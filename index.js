091a47bd74ccdc9910b8a4b196fa769ab67da0b0
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-08-25 00:32:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client({ fetchAllMembers: true, sync: true });
 const config = require('./config.json');
-const client = require("fortnite")
 client.config = config;
 
 client.on("ready", () => {
@@ -546,108 +545,6 @@ client.on("message", async message => {
   message.channel.send(embed) // Sends the embed in the channel
 }
 });
-
-client.on('message', async message => {
-  var request = require('request');
-  
-
-  if(message.content == '+fortnite'){
-
-        let platform = args[0]
-        if(platform) platform = platform.toLowerCase()
-        const username = args.slice(1,args.length).join('%20')
-    
-        if(platform !== 'psn' && platform !== 'pc' && platform !== 'xbl' || platform === undefined){
-          message.reply(`
-          The platform was not set correctly.
-          Please type in commands like so:
-          !stats pc BillyGates
-          !stats xbl MajorNelson
-          !stats psn KazHarai
-          `)
-          return null;
-        }
-        else if(username === undefined){
-          message.reply(`
-          The username was not set.
-          Please type in commands like so:
-          !stats platform username
-          !stats pc BillyGates
-          `)
-          return null;
-        }
-    
-        const options = {
-          'uri': `https://api.fortnitetracker.com/v1/profile/${platform}/${username}`,
-          'headers': {
-            'TRN-Api-Key': process.env.FRTNT,
-            'Content-Type': 'application/json'
-          }
-        }
-    
-        request(options, (err, res, body) => {
-          if(err){
-            console.error(err)
-            message.reply(`There was a problem getting stats for ${username.replace('%20', ' ')} on ${platform}.`)
-            return null
-          }
-          else if(res.statusCode === 200){
-            data = JSON.parse(body)
-            let soloWins = 0
-            let soloTime = '0h 0m'
-            let soloKd = 0
-            let duoWins = 0
-            let duoTime = '0h 0m'
-            let duoKd = 0
-            let squadWins = 0
-            let squadTime = '0h 0m'
-            let squadKd = 0
-    
-            // Player Not Found
-            if(data.error){
-              msg.reply(`${data.error}`)
-              return null
-            }
-    
-            // Solo
-            if(data.stats.p2){
-              soloWins = data.stats.p2.top1.displayValue
-              soloTime = data.stats.p2.minutesPlayed.displayValue
-              soloKd = data.stats.p2.kd.valueDec
-            }
-    
-            // Duos
-            if(data.stats.p10){
-              duoWins = data.stats.p10.top1.displayValue
-              duoTime = data.stats.p10.minutesPlayed.displayValue
-              duoKd = data.stats.p10.kd.valueDec
-            }
-            
-            // Squad 
-            if(data.stats.p9){
-              squadWins = data.stats.p9.top1.displayValue
-              squadTime = data.stats.p9.minutesPlayed.displayValue
-              squadKd = data.stats.p9.kd.valueDec
-            }
-    
-            // The most important line of code.
-            const user = username.replace(/b/ig, '🅱').replace('%20', ' ')
-    
-            message.reply(`
-            Stats for ${user} on ${platform}:
-            Gametype | # of Wins | Time Played | KD Ratio
-            Solo | ${soloWins} | ${soloTime} | ${soloKd}
-            Duo | ${duoWins} | ${duoTime} | ${duoKd}
-            Squad | ${squadWins} | ${squadTime} | ${squadKd}
-            Source: FORTNITE TRACKER - https://fortnitetracker.com/
-            `)
-          } 
-          else {
-            message.reply(`Something went wrong. Sorry :b:ud.`)
-            return null
-          }
-        })}})
-        
 
 client.on('message', (message) => {
     if(message.content == '+help'){
