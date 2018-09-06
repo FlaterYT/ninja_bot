5cbf333aefa77f8981b4140bf327963cdcbe3048
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-09-06 14:46:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -598,10 +598,9 @@ client.on("message", async message => {
   }
   //Fortnite stats
   //let username = args[0]; //Gets username
-  let username = args.slice(0,args.length).join('%20');
-  let platform = args[1]
-  if(platform) platform = platform.toLowerCase()
-  let mode = "life"; //Default stats: lifetime
+  let platform = args[0]
+    if(platform) platform = platform.toLowerCase()
+    const username = args.slice(1,args.length).join('%20')
 
   if (args[2]) {
     if (args[2].toLowerCase() == "all" || args[2].toLowerCase() == "season") {
