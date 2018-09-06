da3781ca6542508451b30a027385c174d195d96e
FlaterYT <38140383+FlaterYT@users.noreply.github.com>
2018-09-06 14:55:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -416,7 +416,7 @@ client.on('message', (message) => {
 
 client.on('message', (message) => {
     if(message.content == '+ligma'){
-        var text = "You have " + Math.floor((Math.random() * 101) + 0) + "% ligma <:GWqlabsArmLeft:463123955772030976> <:ninja:480134047969050634> <:vbucksarmright:467371808782417940>";
+        var text = "You have " + Math.floor((Math.random() * 100) + 0) + "% ligma <:GWqlabsArmLeft:463123955772030976> <:ninja:480134047969050634> <:vbucksarmright:467371808782417940>";
         message.channel.send({embed: {
         color: 15844367,
         description: text
@@ -452,11 +452,11 @@ client.on('message', (message) => {
 
 var taggedmember1 = message.mentions.members.first();
 
-		var text = (taggedmember1) + " has " + Math.floor((Math.random() * 101) + 0) + "% ligma <:GWqlabsArmLeft:463123955772030976> <:ninja:480134047969050634> <:vbucksarmright:467371808782417940>";
+		var text = (taggedmember1) + " has " + Math.floor((Math.random() * 100) + 0) + "% ligma <:GWqlabsArmLeft:463123955772030976> <:ninja:480134047969050634> <:vbucksarmright:467371808782417940>";
 		
 		var messagecontentxd = message.content;
 		var text1 = messagecontentxd.replace('+ligma ','');
-		var textD = (text1) + " has " + Math.floor((Math.random() * 101) + 0) + "% ligma <:GWqlabsArmLeft:463123955772030976> <:ninja:480134047969050634> <:vbucksarmright:467371808782417940>";
+		var textD = (text1) + " has " + Math.floor((Math.random() * 100) + 0) + "% ligma <:GWqlabsArmLeft:463123955772030976> <:ninja:480134047969050634> <:vbucksarmright:467371808782417940>";
 
 		
 		
@@ -597,10 +597,9 @@ client.on("message", async message => {
     return message.channel.send(places[picker]); //Sends randomely picked spot
   }
   //Fortnite stats
-  //let username = args[0]; //Gets username
-  let platform = args[1]
-    if(platform) platform = platform.toLowerCase()
-    let username = args.slice(0,args.length).join('%20')
+  let username = args[0]; //Gets username
+  let platform = args[1] || "pc"; //Gets platform, default: pc
+  let mode = "life"; //Default stats: lifetime
 
   if (args[2]) {
     if (args[2].toLowerCase() == "all" || args[2].toLowerCase() == "season") {
